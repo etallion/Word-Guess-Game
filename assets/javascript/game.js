59fd86fc7608a8fe3457b934eb6f63a25673c742
Will Mangimelli <willwinman@gmail.com>
2019-03-23 12:49:40 -0500

    changes to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+ // JavaScript function that wraps everything
+ //$(document).ready(function() {
+
 var game = {
     answers : ["Taladega Nights", 
     "Elf", 
@@ -10,16 +13,80 @@ var game = {
     alphabet:["a","b", "c", "d", "e", "f", "g",
     "h","i","j","k","l","m","n","o","p","q","r",
     "s","t","u","v","w","x","y","z"],
-    useLetters:[],
-    tries:0,
-    maxAttempts:9,
+    usedLetters:[],
+    guessesLeft:0,
     activeAnswer: "",
-    checkLetter: function(letter){
-        if (game.activeAnswer.indexOf(letter) > -1) {
-            console.log("Letter Guessed Correct")
-        } else {
-            console.log("WRONG");
+    isCharHidden: [],
+    charFound: function(letter){
+
+        //parse activeAnswer for position(s) of matching character
+       for(var i = 0; i < game.activeAnswer.length; i++){
+       
+            if(game.activeAnswer.charAt(i) === letter || game.activeAnswer.charAt(i)  === letter.toUpperCase()){
+                game.isCharHidden[i] = false;
+            }
+       }
+       $("#hiddenWord").html(game.updatedDisplay());
+        
+        //check to see if puzzle solved by looking for all true values in isCharHidden
+        var boolGame = game.checkForWinner(game.isCharHidden[game.isCharHidden.length-1]);
+        console.log("boolGame: " + boolGame);
+        if(boolGame){
+            alert("You WON!!");
         }
+       
+    },
+    charNotFound: function(letter){
+        
+        game.guessesLeft--;
+        $("#guessesLeft").html("Guesses left: " + game.guessesLeft);
+
+        if(game.guessesLeft > 0){
+            $("#wrongLetters").append(letter + " ");
+        } else {
+                alert("Game Over!")
+        }
+    },
+    updatedDisplay: function(){
+
+        var str = "";
+
+        game.isCharHidden.forEach(function(bool, i){
+            if(bool){
+                console.log("true");
+                str += "_ ";
+            } else {
+                console.log(i);
+                console.log("false");
+                str = str + game.activeAnswer.charAt(i);
+                str += " ";
+            }
+        });
+        return str;
+    },
+    checkLetter: function(letter){
+        if (game.activeAnswer.indexOf(letter) > -1 || game.activeAnswer.indexOf(letter.toUpperCase()) > -1) {
+            console.log("found ...");
+            game.charFound(letter);
+        } else {
+            console.log("not found ...");
+            game.charNotFound(letter);
+        }
+    },
+    checkForWinner: function(i){
+        console.log("check for winner");
+        if(game.isCharHidden[i] == false){
+            console.log(i + " is false");
+            if( i === 0){
+                //alert("WINNER");
+                return true;
+            } else {
+                i--;
+                console.log("i now = " + i + " check again");
+                game.checkForWinner(i);
+            }
+        } else 
+        return false;
     }
 };
 
@@ -28,48 +95,74 @@ document.onkeyup = function(evet){
     var userGuess = event.key.toLocaleLowerCase();
 
     if (game.alphabet.indexOf(userGuess) > -1) {
-        console.log("user guessed letter: " + userGuess);
-        game.checkLetter(userGuess);
-        $("#wrongLetters").append(userGuess + " ");
+        console.log("used letter position: " + game.usedLetters.indexOf(userGuess));
+        if(game.usedLetters.indexOf(userGuess) < 0){
+            game.usedLetters.push(userGuess);
+            console.log("user guessed letter: " + userGuess);
+            game.checkLetter(userGuess);
+        } 
     } else if(event.key.toLocaleLowerCase() == '1'){
-        startfGame();
+         init();
     }
 }
 
-function startfGame(){
+function init(){
     // rand number, modulus length of game.answers
     var secretAnswer = game.answers[Math.floor(Math.random() * game.answers.length)];
+    
+    // set active answer
     game.activeAnswer = secretAnswer;
-    //display _ _ _ _ unscores with space for each letter in answer the user will be guessing
-    var hiddenWord = '';
-    for( var i = 0; i < secretAnswer.length; i++){
-       hiddenWord = hiddenWord + "_ "; 
-    }
-   console.log(hiddenWord);
+    game.guessesLeft = 9; 
 
-   $("#hiddenWord").html(hiddenWord);
+    // init isCharHidden array all to false
+    for( var i = 0; i < game.activeAnswer.length; i++){
+
+        //unhide non-alaphbet chars
+        if(game.activeAnswer.charAt(i) === " " || game.activeAnswer.charAt(i) === "'"){
+            game.isCharHidden[i] = false;
+        } else { // hide letter
+            game.isCharHidden[i] = true;
+        }
+    }
+
+    //display game spaces like this _ _ _ _  
+    var display = game.updatedDisplay();
+    $("#hiddenWord").html(game.updatedDisplay());
+    
+    // initialize wrong letter 
+    $("#wrongLetters").html("");
+
+    // initialize guess left 
+    $("#guessesLeft").html("Guesses left: " + game.guessesLeft);
 
    // jQuery alternative to: var newDiv = document.createElement("div");
    var newDiv = $("<div id=wLetters>");
 
    // jQuery alternative to: newDiv.textContent = "A pleasure to meet you!";
-   newDiv.text("p");
+   newDiv.text("");
 
    // jQuery alternative to: document.querySelector("#empty-div").appendChild(newDiv);
    $("#wrongLetters").append(newDiv);
 
-    //wait user input for letter
-    secretAnswer.charAt
-    //user types key now handle 
+};
+//END OF INIT()
 
-    //user picks right letter
+// Gets Link for Theme Song
+ var audioElement = document.createElement("audio");
+ audioElement.setAttribute("src", "assets/myboyblue.mp3");
 
-    //user solved word?
+ var audioElement2 = document.createElement("audio");
+ audioElement2.setAttribute("src", "assets/ilikeyou.mp3");
 
-    //pick wrong letter
+ // Theme Button
+ $(".theme-button").on("click", function() {
+    console.log("jk");
+   audioElement.play();
+ });
 
-    //user lost
+ $(".pause-button").on("click", function() {
+    console.log("pause");
+   audioElement2.play();
+ });
 
-
-}
-
+//});
